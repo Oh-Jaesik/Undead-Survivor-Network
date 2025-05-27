2e54d338c6c124c9ab09abb4565c2606b5770671
jaesik <ojs72272@gmail.com>
2025-05-27 19:21:27 +0900

    player별 target 설정 변경

--- Assets/Undead Survivor/Codes/Enemy.cs
+++ Assets/Undead Survivor/Codes/Enemy.cs
@@ -1,3 +1,181 @@
+//using Mirror;
+//using System.Collections;
+//using UnityEngine;
+
+//public class Enemy : NetworkBehaviour
+//{
+//    public float speed;
+
+//    [SyncVar]
+//    public float health;
+//    [SyncVar]
+//    public float maxHealth;
+//    [SyncVar(hook = nameof(OnSpriteTypeChanged))]
+//    public int spriteType;
+//    [SyncVar]
+//    public bool isLive;
+
+//    [SyncVar(hook = nameof(OnDeadStateChanged))]
+//    public bool isDead;
+
+//    public RuntimeAnimatorController[] animCon;
+//    public Rigidbody2D target;
+//    WaitForFixedUpdate wait;
+
+//    [SyncVar(hook = nameof(OnEnemyFlipXChanged))]
+//    public bool currentFlipXState;
+
+//    Rigidbody2D rigid;
+//    Collider2D coll;
+//    Animator anim;
+//    SpriteRenderer spriter;
+
+//    void Awake()
+//    {
+//        rigid = GetComponent<Rigidbody2D>();
+//        coll = GetComponent<Collider2D>();
+//        anim = GetComponent<Animator>();
+//        spriter = GetComponent<SpriteRenderer>();
+//        wait = new WaitForFixedUpdate();
+//    }
+
+//    public override void OnStartServer()
+//    {
+//        if (isServer)
+//        {
+//            isLive = true;
+//            isDead = false;
+//            coll.enabled = true;
+//            rigid.simulated = true;
+//            spriter.sortingOrder = 2;
+//            anim.SetBool("Dead", false);
+//            health = maxHealth;
+//            StartCoroutine(FindTargetWithDelay(0.7f));
+//        }
+//    }
+
+//    IEnumerator FindTargetWithDelay(float delay)
+//    {
+//        yield return new WaitForSeconds(delay);
+
+//        if (isServer)
+//        {
+//            target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+//        }
+//    }
+
+//    void FixedUpdate()
+//    {
+//        if (!isServer || !isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
+//            return;
+
+//        if (target != null)
+//        {
+//            Vector2 dirVec = target.position - rigid.position;
+//            Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+//            rigid.MovePosition(rigid.position + nextVec);
+//            rigid.linearVelocity = Vector2.zero;
+
+//            bool newFlipX = target.position.x < rigid.position.x;
+//            if (newFlipX != currentFlipXState)
+//            {
+//                currentFlipXState = newFlipX;
+//            }
+//        }
+//    }
+
+//    void OnEnemyFlipXChanged(bool oldFlipXState, bool newFlipXState)
+//    {
+//        spriter.flipX = newFlipXState;
+//    }
+
+//    void OnSpriteTypeChanged(int oldSpriteType, int newSpriteType)
+//    {
+//        if (anim != null && animCon.Length > newSpriteType && newSpriteType >= 0)
+//        {
+//            anim.runtimeAnimatorController = animCon[newSpriteType];
+//        }
+//    }
+
+//    void OnDeadStateChanged(bool oldValue, bool newValue)
+//    {
+//        if (anim != null)
+//        {
+//            anim.SetBool("Dead", newValue);
+//            coll.enabled = !newValue;
+//            rigid.simulated = !newValue;
+//            spriter.sortingOrder = newValue ? 1 : 2;
+//        }
+//    }
+
+//    [Server]
+//    public void Init(SpawnData data)
+//    {
+//        speed = data.speed;
+//        spriteType = data.spriteType;
+//        maxHealth = data.health;
+//        health = data.health;
+//        isLive = true;
+//        isDead = false;
+//    }
+
+//    void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if (!collision.CompareTag("Bullet") || !isLive)
+//            return;
+
+//        if (!isServer) return;
+
+//        health -= collision.GetComponent<Bullet>().damage;
+//        StartCoroutine(KnockBack());
+
+//        if (health > 0)
+//        {
+//            RpcPlayHitAnim();
+//        }
+//        else
+//        {
+//            isLive = false;
+//            isDead = true;
+
+//            StartCoroutine(DelayedDead());
+//        }
+//    }
+
+//    [Server]
+//    IEnumerator KnockBack()
+//    {
+//        yield return wait;
+
+//        Vector3 playerPos = GameManager.instance.player.transform.position;
+//        Vector3 dirVec = transform.position - playerPos;
+//        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+//    }
+
+//    [ClientRpc]
+//    void RpcPlayHitAnim()
+//    {
+//        if (anim != null)
+//            anim.SetTrigger("Hit");
+//    }
+
+//    [Server]
+//    IEnumerator DelayedDead()
+//    {
+//        yield return new WaitForSeconds(1f); // 죽는 애니메이션 재생 시간
+
+//        Dead();
+//    }
+
+//    [Server]
+//    void Dead()
+//    {
+//        // 오브젝트 풀에 반환 전에 상태 초기화 필요 시 추가 가능
+//        GameManager.instance.pool.ReturnToPool(gameObject);
+//    }
+//}
+
+
 using Mirror;
 using System.Collections;
 using UnityEngine;
@@ -60,7 +238,9 @@ public class Enemy : NetworkBehaviour
 
         if (isServer)
         {
-            target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+            Player nearestPlayer = GameManager.instance.GetNearestPlayer(transform.position); // 가장 가까운 플레이어
+            if (nearestPlayer != null)
+                target = nearestPlayer.GetComponent<Rigidbody2D>();
         }
     }
 
@@ -137,7 +317,6 @@ public class Enemy : NetworkBehaviour
         {
             isLive = false;
             isDead = true;
-
             StartCoroutine(DelayedDead());
         }
     }
@@ -147,7 +326,8 @@ public class Enemy : NetworkBehaviour
     {
         yield return wait;
 
-        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Player nearestPlayer = GameManager.instance.GetNearestPlayer(transform.position); // 가장 가까운 플레이어
+        Vector3 playerPos = nearestPlayer != null ? nearestPlayer.transform.position : transform.position;
         Vector3 dirVec = transform.position - playerPos;
         rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
     }
@@ -170,7 +350,6 @@ public class Enemy : NetworkBehaviour
     [Server]
     void Dead()
     {
-        // 오브젝트 풀에 반환 전에 상태 초기화 필요 시 추가 가능
         GameManager.instance.pool.ReturnToPool(gameObject);
     }
 }
--- Assets/Undead Survivor/Codes/GameManager.cs
+++ Assets/Undead Survivor/Codes/GameManager.cs
@@ -1,30 +1,78 @@
-using Mirror;
+//using Mirror;
+//using UnityEngine;
+
+//public class GameManager : NetworkBehaviour
+//{
+//    public static GameManager instance;
+
+//    [SyncVar]
+//    public float gameTime;
+//    public float maxGameTime = 2 * 10f;
+
+//    public PoolManager pool;
+//    public Player player;
+
+//    void Awake()
+//    {
+//        instance = this;
+//    }
+
+//    [Server]
+//    void Update()
+//    {
+//        gameTime += Time.deltaTime;
+
+//        if (gameTime > maxGameTime)
+//        {
+//            gameTime = maxGameTime;
+//        }
+//    }
+//}
+
+using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : NetworkBehaviour
+public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    [SyncVar]
-    public float gameTime;
-    public float maxGameTime = 2 * 10f;
-
     public PoolManager pool;
-    public Player player;
+    public float gameTime;
+
+    public List<Player> players = new List<Player>(); // 모든 플레이어 목록
 
     void Awake()
     {
         instance = this;
     }
 
-    [Server]
-    void Update()
+    public void RegisterPlayer(Player player)
     {
-        gameTime += Time.deltaTime;
+        if (!players.Contains(player))
+            players.Add(player);
+    }
 
-        if (gameTime > maxGameTime)
+    public void UnregisterPlayer(Player player)
+    {
+        if (players.Contains(player))
+            players.Remove(player);
+    }
+
+    public Player GetNearestPlayer(Vector3 fromPosition)
+    {
+        Player nearest = null;
+        float minDist = float.MaxValue;
+
+        foreach (Player p in players)
         {
-            gameTime = maxGameTime;
+            float dist = Vector3.Distance(fromPosition, p.transform.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                nearest = p;
+            }
         }
+
+        return nearest;
     }
 }
